7a5a53837299a15e37c2f93e487648dc61f940b8
Farmerjoe12 <afarmelo@yahoo.com>
2017-03-29 12:53:48 -0400

    Upgraded Player rendering and selection

--- 2DGame/core/src/com/mygdx/game/Renderer.java
+++ 2DGame/core/src/com/mygdx/game/Renderer.java
@@ -10,13 +10,14 @@ import com.badlogic.gdx.graphics.GL20;
  *
  */
 public class Renderer {
-
-	private static Player player = new Player();
+	
 	private static Camera cam = new Camera();
 	
 	public static void render() {
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		cam.render();
-		player.render();
+		
 	}
 }
