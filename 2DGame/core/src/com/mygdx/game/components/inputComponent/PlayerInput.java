86e455958dbc220047dd0c670d84c0d5d93a265c
Jacob Kreiner <kreinerjm@hotmail.com>
2017-03-30 14:06:43 -0400

    Removed second call to handleInput() causing movement to be double

--- 2DGame/core/src/com/mygdx/game/components/inputComponent/PlayerInput.java
+++ 2DGame/core/src/com/mygdx/game/components/inputComponent/PlayerInput.java
@@ -12,7 +12,7 @@ public class PlayerInput extends Component implements InputComponent {
 
 	// @param deltaVal controls the amount that the player moves every frame
 	//		while recieving input
-	private float deltaVal = .5f;
+	private float deltaVal = 1f;
 	public static final int UP = 0;
 	public static final int DOWN = 1;
 	public static final int LEFT = 2;
--- 2DGame/core/src/com/mygdx/game/entities/characters/Player.java
+++ 2DGame/core/src/com/mygdx/game/entities/characters/Player.java
@@ -27,7 +27,7 @@ public class Player extends Character
     @Override
     public void tick()
     {
-        getComponent(PlayerInput.class).handleInput();
+
     }
 
 }
