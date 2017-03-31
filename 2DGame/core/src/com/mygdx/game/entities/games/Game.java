0f9367d9d0ee743336108edfd35375bd1a11ba12
indivisibleman <michael.topsom@googlemail.com>
2017-03-31 10:25:35 +0100

    Simplified the checking of map bounds by separately dealing with the horizontal and vertical

--- 2DGame/core/src/com/mygdx/game/entities/games/Game.java
+++ 2DGame/core/src/com/mygdx/game/entities/games/Game.java
@@ -13,90 +13,67 @@ import com.mygdx.game.entities.worlds.TiledWorld;
 /**
  * Created by Jacob on 3/29/2017.
  */
-public abstract class Game extends Entity
-{
-    private OrthographicCamera camera;
+public abstract class Game extends Entity {
+  private OrthographicCamera camera;
 
-    public void setCamera(OrthographicCamera camera) {
-        this.camera = camera;
+  public void setCamera(OrthographicCamera camera) {
+    this.camera = camera;
+  }
+
+  public OrthographicCamera getCamera() {
+    return camera;
+  }
+
+  public abstract void draw(SpriteBatch batch);
+
+  public void updateCamera() {
+
+    float x = getChild(Player.class).getComponent(Transform.class).getPosition().x + 16;
+    float y = getChild(Player.class).getComponent(Transform.class).getPosition().y + 16;
+    camera.position.set(x, y, 0);
+    checkMapBorders();
+    camera.update();
+
+  }
+
+  private void checkMapBorders() {
+    Player player = MyGdxGame.simpleGame.getChild(Player.class);
+    MapProperties prop = getChild(TiledWorld.class).getProp();
+    // get Tiled map properties
+    int mapWidth = prop.get("width", Integer.class);
+    int mapHeight = prop.get("height", Integer.class);
+    int tilePixelWidth = prop.get("tilewidth", Integer.class);
+    int tilePixelHeight = prop.get("tileheight", Integer.class);
+
+    // 'width' and 'height' supply the amount of tiles in each row/column,
+    // so you must multiply it by the tile width in order to get the map dimensions
+    int mapPixelWidth = mapWidth * tilePixelWidth;
+    int mapPixelHeight = mapHeight * tilePixelHeight;
+
+    // determine when the camera will reach the ends of the map
+    float viewportMinX = camera.viewportWidth / 2;
+    float viewportMinY = camera.viewportHeight / 2;
+    float viewportMaxX = mapPixelWidth - viewportMinX;
+    float viewportMaxY = mapPixelHeight - viewportMinY;
+
+    float x = player.getComponent(Transform.class).getPosition().x;
+    float y = player.getComponent(Transform.class).getPosition().y;
+
+    // preventing the camera from scrolling past the edge of the map
+    if (x < viewportMinX) {
+      camera.position.x = viewportMinX;
+    } else if (x > viewportMaxX) {
+      camera.position.x = viewportMaxX;
+    } else {
+      camera.position.x = x;
     }
 
-    public OrthographicCamera getCamera() {
-        return camera;
+    if (y < viewportMinY) {
+      camera.position.y = viewportMinY;
+    } else if (y > viewportMaxY) {
+      camera.position.y = viewportMaxY;
+    } else {
+      camera.position.y = y;
     }
-
-    public abstract void draw(SpriteBatch batch);
-
-    public void updateCamera() {
-
-        float x = getChild(Player.class).getComponent(Transform.class).getPosition().x+16;
-        float y = getChild(Player.class).getComponent(Transform.class).getPosition().y+16;
-        camera.position.set(x,y,0);
-        checkMapBorders();
-        camera.update();
-
-    }
-
-    private void checkMapBorders() {
-        Player player = MyGdxGame.simpleGame.getChild(Player.class);
-        MapProperties prop = getChild(TiledWorld.class).getProp();
-        //get Tiled map properties
-        int mapWidth = prop.get("width", Integer.class);
-        int mapHeight = prop.get("height", Integer.class);
-        int tilePixelWidth = prop.get("tilewidth", Integer.class);
-        int tilePixelHeight = prop.get("tileheight", Integer.class);
-
-        // 'width' and 'height' supply the amount of tiles in each row/column,
-        // so you must multiply it by the tile width in order to get the map dimensions
-        int mapPixelWidth = mapWidth * tilePixelWidth;
-        int mapPixelHeight = mapHeight * tilePixelHeight;
-
-        // determine when the camera will reach the ends of the map
-        float viewportMinX = camera.viewportWidth / 2;
-        float viewportMinY = camera.viewportHeight / 2;
-        float viewportMaxX = mapPixelWidth - viewportMinX;
-        float viewportMaxY = mapPixelHeight - viewportMinY;
-
-        float x = player.getComponent(Transform.class).getPosition().x;
-        float y = player.getComponent(Transform.class).getPosition().y;
-
-        // preventing the camera from scrolling past the edge of the map
-        if (x < viewportMinX && y < viewportMinY)
-        {
-            camera.position.x = viewportMinX;
-            camera.position.y = viewportMinY;
-        }else if(x > viewportMaxX && y > viewportMaxY)
-        {
-            camera.position.x = viewportMaxX;
-            camera.position.y = viewportMaxY;
-        }else if(x > viewportMaxX && y < viewportMinY)
-        {
-            camera.position.x = viewportMaxX;
-            camera.position.y = viewportMinY;
-        }else if(x < viewportMinX && y > viewportMaxY)
-        {
-            camera.position.x = viewportMinX;
-            camera.position.y = viewportMaxY;
-        }else if(x < viewportMinX)
-        {
-            camera.position.x = viewportMinX;
-            camera.position.y = y;
-        }else if(y < viewportMinY)
-        {
-            camera.position.y = viewportMinY;
-            camera.position.x = x;
-        }else if(x > viewportMaxX)
-        {
-            camera.position.x = viewportMaxX;
-            camera.position.y = y;
-        }else if(y > viewportMaxY)
-        {
-            camera.position.y = viewportMaxY;
-            camera.position.x = x;
-        }else{
-            camera.position.x = x;
-            camera.position.y = y;
-        }
-
-    }
+  }
 }
