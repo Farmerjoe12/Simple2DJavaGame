a9212950e8ef4d02dedd5dfa8e18cfa34c345e76
Farmerjoe12 <afarmelo@yahoo.com>
2017-04-08 13:22:07 -0400

    stats finished

--- 2DGame/core/src/com/mygdx/game/components/statComponent/PlayerStatComponent.java
+++ 2DGame/core/src/com/mygdx/game/components/statComponent/PlayerStatComponent.java
@@ -1,32 +1,7 @@
 package com.mygdx.game.components.statComponent;
 
-import com.mygdx.game.components.Component;
 
-public class PlayerStatComponent extends Component{
+public class PlayerStatComponent extends StatComponent{
 	
-	//these values are subject to change
-	int health = 100;
-	int armor = 10;
-	int strength = 5;
-	int abilityPoints = 50;
-	int level = 1;
-	float experience = 100;
-	float moveSpeed = 1f;
-	float carryWeight = strength * 5; // some derivative of strength, subject to change
-	// getters
-	public int getHealth() {return health;}
-	public int getArmor() {return armor;}
-	public int getStrength() {return strength;}
-	public int getAP() {return abilityPoints;}
-	public int getLevel() {return level;}
-	public float getEXP() {return experience;}
-	public float getMoveSpeed() {return moveSpeed;}
-	// setters
-	public int addHealth(int toAdd) {return health += toAdd;}	
-	public int addArmor(int toAdd) {return armor += toAdd;}	
-	public int addStrength(int toAdd) {return armor += toAdd;}	
-	public int setAP(int toAdd) {return abilityPoints += toAdd;}	
-	public int levelUP() {return level++;}	
-	public float addEXP(float toAdd) {return experience += toAdd;}	
-	public float setMoveSpeed(float newSpd) {return moveSpeed = newSpd;}	
+		
 }
