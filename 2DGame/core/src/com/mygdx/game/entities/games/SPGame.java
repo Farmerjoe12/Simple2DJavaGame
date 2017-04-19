e1fdb39bc35062635b5b0b8b23c172698e0acb6b
Farmerjoe12 <afarmelo@yahoo.com>
2017-04-19 12:58:53 -0400

    SPGame commit

---
+++ 2DGame/core/src/com/mygdx/game/entities/games/SPGame.java
@@ -0,0 +1,43 @@
+package com.mygdx.game.entities.games;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.components.physicsComponent.Transform;
+import com.mygdx.game.entities.characters.Enemy;
+import com.mygdx.game.entities.characters.Player;
+import com.mygdx.game.entities.worlds.TiledWorld;
+
+/**
+ * Created by Jacob on 3/29/2017.
+ */
+public class SPGame extends Game{
+
+    /**
+     * SPGame when instantiated sets this game to the current game
+     *  and sets up the camera and input to track within this game
+     */
+    
+    public SPGame() {
+	MyGdxGame.currentGame = this;
+	TiledWorld tiledWorld = new TiledWorld();
+	addChild(tiledWorld);
+	Player player = new Player();
+	Enemy enemy = new Enemy();
+	addChild(enemy);
+	player.getComponent(Transform.class).setPosition(new Vector2(450f, 900f));
+	addChild(player);
+    }
+    
+    public void draw(SpriteBatch batch) {
+	updateCamera();
+	batch.setProjectionMatrix(getCamera().combined);
+	getChild(TiledWorld.class).drawBackground(batch);
+	batch.begin();
+	getChild(Player.class).draw(batch);
+	getChild(Enemy.class).draw(batch);
+	batch.end();
+	getChild(TiledWorld.class).drawForeground(batch);
+    }
+
+}
